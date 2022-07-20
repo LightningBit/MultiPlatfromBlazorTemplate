0af7a3e832900707cd879c77012037ea1c3a11b6
LightningBits <LIghtnigbits@lightningbits.com>
2022-07-20 15:13:49 -0400

    Added Tailwind CSS to project

---
+++ BlazorMobile/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    //mode: "Jit"
+    content: ["./**/*.{razor,html,cshtml}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
