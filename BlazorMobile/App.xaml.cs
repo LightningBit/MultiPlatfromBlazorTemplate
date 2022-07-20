6d071cba0874b71d6fb6640bfb026d8db9039173
LightningBits <LIghtnigbits@lightningbits.com>
2022-07-19 23:08:02 -0400

    Step one add Dotnet MauiBlazor
    Clean up app, target only IOS and Android. Test app load up emulators.

---
+++ BlazorMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorMobile;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
+
---
+++ BlazorMobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorMobile;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
+
