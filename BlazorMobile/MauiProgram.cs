0db615731228cd372439ce83331f1b051d67efd2
LightningBits <LIghtnigbits@lightningbits.com>
2022-07-21 15:03:50 -0400

    Added Html Tailwind templates
    tested on Mobile all seems well.

--- BlazorMobile/MauiProgram.cs
+++ BlazorMobile/MauiProgram.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Components.WebView.Maui;
-using BlazorMobile.Data;
 
 namespace BlazorMobile;
 
@@ -20,7 +19,6 @@ public static class MauiProgram
         builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
 
-        builder.Services.AddSingleton<WeatherForecastService>();
 
         return builder.Build();
     }
